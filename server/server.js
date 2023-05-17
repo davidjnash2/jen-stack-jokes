d34dd6b194df30a2404777da400eabd82e94760c
Dave Nash <davidjnash2@gmail.com>
2023-05-17 18:45:37 -0500

    commented serverjs doc

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = 5000;
 // use bodyParser.urlencoded throughout the app with this:
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let jokes = [
+let jokes = [ // declared jokes array
   {
     whoseJoke: "Danny",
     jokeQuestion: "Why do scuba divers fall backwards out of boats?",
@@ -34,34 +34,34 @@ let jokes = [
   }
 ];
 
-console.log('jokes are:', jokes)
+console.log('jokes are:', jokes) // log to test
 
 // serve back static files
 app.use(express.static('server/public'));
 
-app.get('/jokes', function(req,res) {
-  console.log('GET /jokes request made', jokes);
-  res.send(jokes);
-  })
+app.get('/jokes', function(req,res) { // start GET
+  console.log('GET /jokes request made', jokes); // log to test
+  res.send(jokes); // send back jokes array as result
+}) // end GET
 
 
-  
-app.post('/jokes', function(req,res) {
-  let whoseJoke = req.body.whoseJoke;
-  let jokeQuestion = req.body.jokeQuestion;
-  let punchLine = req.body.punchLine;
 
-  console.log('POST /jokes request made');
-  console.log('req.body is :', req.body);
+app.post('/jokes', function(req,res) { // start POST
+  let whoseJoke = req.body.whoseJoke; // declare variable for incoming parsed data
+  let jokeQuestion = req.body.jokeQuestion; // declare variable for incoming parsed data
+  let punchLine = req.body.punchLine; // declare variable for incoming parsed data
 
-  let joke = {
+  console.log('POST /jokes request made'); // log to test
+  console.log('req.body is :', req.body); // log to test
+
+  let joke = { // declare joke object using above values for inputs
     whoseJoke: whoseJoke,
     jokeQuestion: jokeQuestion,
     punchLine: punchLine
     }
-  jokes.push(joke);
-  res.sendStatus(201);
-})
+  jokes.push(joke); // push new jokes to array
+  res.sendStatus(201); // send ok to client
+}) // end POST
 
 
 
